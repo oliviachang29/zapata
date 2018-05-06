771e237782548765f49180dce4167a7fe7e9e834
Olivia <cozymonster29@gmail.com>
2018-05-05 17:25:30 -0700

    add spark

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
+jQuery.expr.filters.offscreen = function(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+           (rect.x + rect.width) < 0 
+             || (rect.y + rect.height) < 0
+             || (rect.x > window.innerWidth || rect.y > window.innerHeight)
+         );
+};
+
 jQuery(document).ready(function($){
 	var contentSections = $('.cd-section'),
 		navigationItems = $('#cd-vertical-nav a');
-        header = $('header'),
+    header = $('header'),
+    map = $('#map-c'),
+    map_texts = $('.spark-indv-text-container'),
+    bigfour = $('#section5'),
+    bigfour_top = bigfour.offset().top,
+
 	// navigationItems.hide();
   // contentSections.hide();
-
-  $(window).scroll(function(){
-    header.css("opacity", 1 - $(window).scrollTop() / 500);
+  map.stick_in_parent({
+    offset_top: $(window).height()/5
   });
 
 	updateNavigation();
@@ -22,34 +35,15 @@ jQuery(document).ready(function($){
 
     //smooth scroll to second section
     $('#begin-button').on('click', function(event){
-      // event.preventDefault();
-      
-      // function contentFadeIn(){
-      //   contentSections.fadeIn();
-      //   navigationItems.fadeIn();
-      //   updateNavigation();
-      // };
-
-      // window.setTimeout( contentFadeIn, 1000 );
+      contentSections.show();
       event.preventDefault();
       smoothScroll($(this.hash));
-      // header.fadeOut();
     });
 
     //smooth scroll to second section
     $('#scroll-down').on('click', function(event){
       event.preventDefault();
       smoothScroll($(this.hash));
-    });
-
-    //open-close navigation on touch devices
-    $('.touch .cd-nav-trigger').on('click', function(){
-    	$('.touch #cd-vertical-nav').toggleClass('open');
-  
-    });
-    //close navigation on touch devices when selectin an elemnt from the list
-    $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
     });
 
 	function updateNavigation() {
@@ -72,11 +66,52 @@ jQuery(document).ready(function($){
 	}
 });
 
+
 $(document).scroll(function() {
+  // header disappears on scroll
+  header.css("opacity", 1 - $(window).scrollTop() / 500);
+
   var y = $(this).scrollTop();
   if (y > $(window).height() - 10) {
     $('#cd-vertical-nav').fadeIn();
   } else {
     $('#cd-vertical-nav').fadeOut();
   }
+
+  if (y > bigfour_top - 500) {
+    $('#map-c').fadeOut();
+  } else {
+    $('#map-c').fadeIn();
+  }
+
+  // map_texts.each(function(){
+  //   $this = $(this);
+  //   var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+  //   if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+  //     navigationItems.eq(activeSection).addClass('is-selected');
+  //   }else {
+  //     navigationItems.eq(activeSection).removeClass('is-selected');
+  //   }
+  // });
+
+  if ((!$('#spark-1').is(':offscreen')) || $('#dot-1').is(":hover")) {
+    $('#gone-1').css({
+        opacity: '1'
+    });
+  } else {
+    $('#gone-1').css({
+        opacity: '0'
+    });
+  }
+
+  if (!$('#spark-2').is(':offscreen')) {
+    $('#gone-2').css({
+        opacity: '1'
+    });
+  } else {
+    $('#gone-2').css({
+        opacity: '0'
+    });
+  }
+  
 });
